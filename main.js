fdbb35d34ad4182903bfe5f0eb0afc46c597d5ba
CloudPwn <35544281+CloudFTL@users.noreply.github.com>
2018-03-04 13:25:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-//- Using a function pointer:
-document.getElementById("clickMe").onclick = doFunction;
 
-//- Using an anonymous function:
-document.getElementById("clickMe").onclick = function () { alert('Created By CloudPWN Exploit Made With iMessage Bug'); };
-var el = document.getElementById("clickMe");
-if (el.addEventListener)
-    el.addEventListener("click", doFunction, false);
-else if (el.attachEvent)
-    el.attachEvent('onclick', doFunction);
-  CapacityWindow.document.write(s);
- CapacityWindow.document.open("text/html");
-CapacityWindow.document.write(s);
-CapacityWindow.document.close();
