ea4e45166dcf4a039fef6fa28b834d761d0cfb66
CloudPwn <35544281+CloudFTL@users.noreply.github.com>
2018-03-03 23:43:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-(function($){
-function loadPage(sel, p){
-        $(sel).load(p + ' #content', function(){
-            document.title = $('#pageData').data('title');
-        });
-   $(document).on('click', 'a', function(e){
-        e.preventDefault();
-        history.pushState({page: this.href}, '', this.href);
-        loadPage('#frontPage', this.href);
-         $(window).on('popstate', function(e){
-        loadPage('#frontPage', location.pathname);
-        console.log(e.originalEvent.state);
-    });
-}(jQuery));
+var createHistory = require('history').createBrowserHistory
