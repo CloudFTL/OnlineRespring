8ca5081e9b53fb65c4bb4557fc414682a7b72016
Cloud <35544281+CloudFTL@users.noreply.github.com>
2018-06-10 13:45:17 -0400

    Update detect.js

--- detect.js
+++ detect.js
@@ -7,10 +7,10 @@
 
 var OSName="Unknown OS";
 if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="iOS/MacOS";
 if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
 if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 
 document.write('Your OS: '+OSName);
 
-window.alert("You Running iOS 11 right i hope so if not then why are you here");
+
